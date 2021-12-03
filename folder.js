d9fd8734173edae4359547a23a07753b8112c248
academy_learner <academy_learner@academy-learners-MacBook-Air.local>
2021-12-03 13:10:13 +0200

    hi

--- folder.js
+++ folder.js
@@ -1,2 +1,45 @@
+/*
+ var number1 = 2;
+ var number2 = 4; 
 
-console.log("MY NAME IS LEE");
+ var avg;
+ avg=(number2+number1 )/2;
+
+ console.log( "My avarage is " ,avg );
+
+var i=[ 2,5,10,3,7,5,9,8,6,1];
+var sum = 0;
+
+ for( i=0; i<=10; i++)
+ {
+
+ sum+=i;
+
+ }
+
+ console.log( "My sum is ", sum);*/
+
+
+
+
+ var Students=["stud1", "stud2","stud3"];
+ var mark1 = [ 30,15,40];
+ var mark2 =[ 50, 25, 5 ];
+ var mark3 = [ 70, 55, 60];
+
+
+ var sum;
+ var avg;
+
+for (var i = 0; i<3; i++ )
+{
+
+sum = mark1[i]+mark2[i]+mark3[i];
+avg=sum/3;
+
+
+
+console.log("The avg for marks is " ,Students[i],"is",avg);
+
+
+}
